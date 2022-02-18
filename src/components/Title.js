26d29c695f6362c84d6d0b05a30e4b157b6d3304
FlavioVillar <flavio_villar@hotmail.com>
2022-02-18 16:56:29 -0300

    Requisito 4 ok - Renderize o componente Title dentro do componente SolarSystem

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,6 +4,7 @@ import PropType from 'prop-types';
 class Title extends React.Component {
   render() {
     const { headline } = this.props;
+    console.log(headline);
     return <h2>{headline}</h2>;
   }
 }
